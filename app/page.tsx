034577036f0581f4e288b51803a34be648e32edd
Kshitijkumar <kshitijkumar1610@gmail.com>
2026-04-15 18:23:41 +0530

    formatted using prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const RootPage = () => {
-  return (
-    <div className='flex flex-col flex-1 items-center justify-center bg-zinc-950 h-screen w-screen gap-y-3'>
-      <p className='text-2xl font-medium text-zinc-100'>Artisanals</p>
-      <p className='text-lg text-zinc-400'>Coming Soon</p>
-    </div>
-  )
-}
+    return (
+        <div className="flex h-screen w-screen flex-1 flex-col items-center justify-center gap-y-3 bg-zinc-950">
+            <p className="text-2xl font-medium text-zinc-100">Artisanals</p>
+            <p className="text-lg text-zinc-400">Coming Soon</p>
+        </div>
+    );
+};
 
-export default RootPage
+export default RootPage;
