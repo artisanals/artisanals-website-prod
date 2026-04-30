f8b00486d0c40c9e56d86cb92b2fb4987b008886
Kshitijkumar <kshitijkumar1610@gmail.com>
2026-04-30 23:26:21 +0530

    changes: starting the admin page with nav

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,12 @@
 const AdminPage = () => {
-    return <div className="bg-white h-screen w-screen">
-        <div className="text-red-500">
-            this is the admin page
+    // this page will act like a dashboard with stats and links for stuff,
+    // for now, we dont know what all it will have, so right now im just gonna add a nav here for brand-brief and brand-identity
+
+    return (
+        <div className="h-screen w-screen bg-white">
+            <div className=""></div>
         </div>
-    </div>;
+    );
 };
 
 export default AdminPage;
